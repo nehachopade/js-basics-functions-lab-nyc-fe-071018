8df53089c33d6cf40dd5306c531a617b2f327504
Neha Chopade <nehachopade22@gmail.com>
2018-08-19 20:23:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(location){
+	let NumberOfBlocks =  Math.abs(42-location);
+	return NumberOfBlocks;
+
+}
+
+function distanceFromHqInFeet(location){
+	let distanceInFeet = ((distanceFromHqInBlocks(location))*264);
+	return distanceInFeet;
+	
+}
+
+function distanceTravelledInFeet(start,end){
+	let x= Math.abs(end-start);
+	let distanceTravelled = x*264;
+	return distanceInFeet;
+
+}
+
+function calculatesFarePrice(start, end){
+	let distance = end-start;
+	let fare;
+	if  (distance <400){
+		fare=0;
+	} else if (distance>=401 && distance<=2000)
+	{
+		fare=distance*0.02;
+	} else if (distance>=2001 && distance<=2500)
+	{
+		fare= Flat Fare
+	} else if (distance >2501)
+	{
+		return 'Cannot travel that far'
+	}
+
+}
